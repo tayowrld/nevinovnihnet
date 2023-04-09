fa8094095882ad4da724fe401779d070a6c9b874
tayowrld <127036325+tayowrld@users.noreply.github.com>
2023-04-09 04:36:12 +0300

    Add files via upload

---
+++ javascript/dinamics.js
@@ -0,0 +1,74 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+
+
+
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+const popup = document.querySelector(".container__popup");
+
+function close_popup(){
+  popup.setAttribute("style","visibility:hidden; opacity: 0;");
+}
+
+function open_popup(){
+  popup.setAttribute("style","visibility:visible; opacity:1;");
+}
+
+
+
+const bookerName = document.getElementById("Name");
+const bookerTel = document.getElementById("Telephone");
+const bookerEmail = document.getElementById("E-mail");
+const bookingDate = document.getElementById("DataTime");
+const bookerInfo = document.getElementById("Other");
+const submit = document.getElementsByClassName('container__popup')[0];
+
+
+
+submit.addEventListener('submit',(e)=>{
+  e.preventDefault();
+
+  let ebody = "<p>Имя: </p>" + bookerName.value + "<br><p>Телефон: </p>" + bookerTel.value + "<br><p>Электронная Почта: </p>" + bookerEmail.value + "<br><p>Дата: </p>" + bookingDate.value + "<br><p>Пожелания: </p>" + bookerInfo.value + "<br>"
+
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "vinonetbar@gmail.com",
+    Password : "4A4DF453AE9B0F3F5D5E39809CD9F5C62ADC",
+    To : 'vinonetbar@gmail.com',
+    From : "vinonetbar@gmail.com",
+    Subject : "Бронь от " + bookerName.value,
+    Body : ebody
+  });
+});
+
+
+
